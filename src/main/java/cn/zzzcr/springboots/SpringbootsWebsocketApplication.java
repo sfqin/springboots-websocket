8806e647995b820e8d72f58ab82227fa0a9a0147
fidoqin <fidoqin@wesure.cn>
2019-04-09 20:50:31 +0800

    .

---
+++ src/main/java/cn/zzzcr/springboots/SpringbootsWebsocketApplication.java
@@ -0,0 +1,13 @@
+package cn.zzzcr.springboots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsWebsocketApplication.class, args);
+	}
+
+}
